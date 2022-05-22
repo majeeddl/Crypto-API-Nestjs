adaf8839f9345ccbda46c278c8e7a4a664378465
Majeed <majeed.dl@gmail.com>
2022-05-22 22:42:31 +0300

    add ccxt and test something

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { exchanges, bybit } from 'ccxt';
+
+const exchangeId = 'bybit';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<any> {
+    const exchange = new bybit({
+      apiKey: 'X1OgT46GUEnxxUdPBT',
+      secret: '86CQrr3EexkDy3gv8lrLmNrMsrioTyP3ehpH',
+      enableRateLimit: true,
+      options: {
+        defaultType: 'futures',
+        adjustForTimeDifference: true,
+      },
+    });
+
+    const data = await exchange.fetchUsedBalance();
+    return {
+      data,
+    };
   }
 }
