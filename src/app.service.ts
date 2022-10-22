a9d1b5bf8d05adcacb15d322ecc38735faf5e0c6
majeed <majeed.dl@gmail.com>
2022-10-22 23:37:57 +0300

    begin working on  get websocket data

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import * as WebSocket from 'ws';
 
 @Injectable()
 export class AppService {
+  private ws = new WebSocket('wss://stream.bybit.com/realtime');
+
+  constructor() {
+    this.ws.on('open', () => {
+      this.ws.send(Math.random());
+    });
+
+    this.ws.on('message', function (message) {
+      console.log(message);
+    });
+  }
   getHello(): string {
     return 'Hello World!';
   }
+
+  send(data: any) {
+    this.ws.send(data);
+  }
+
+  onMessage(handler: Function) {}
 }
